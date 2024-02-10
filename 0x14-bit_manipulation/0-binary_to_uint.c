63ac0f88a3d6a7a78512c8c7e05835dc195ed806
Judyannie96 <judithochuba@gmai.com>
2024-02-09 20:52:28 -0800

    2-get_bit.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,13 +7,14 @@
  *
  * Return: unsigned int.
  */
-unsigned int binary_to_uint(const char *b);
+unsigned int binary_to_uint(const char *b)
 {
 	unsigned int ui;
 	int len, base_two;
 
 	if (!b)
 		return (0);
+
 	ui = 0;
 
 	for (len = 0; b[len] != '\0'; len++)
@@ -25,6 +26,7 @@ unsigned int binary_to_uint(const char *b);
 		{
 			return (0);
 		}
+
 		if (b[len] & 1)
 		{
 			ui += base_two;
